e61d90fa3e95e5d6bfea433ee3c309b70d905ae0
Nassuka <135132361+Nassuka@users.noreply.github.com>
2023-07-28 16:34:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import plotly.express as px
+#import plotly.express as px
 
 #from fonctions import calcul_conso_gpl,rendement, calcul_energy_hfo, price_hfo, price_gpl, euro_to_dollar, dollar_to_CFA, dollar_to_ZAR, dollar_to_din_tun, dollar_to_mur, space_in_numbers, courbe
 #from gen_pdf import gen_pdf
-from bokeh.plotting import figure
+#from bokeh.plotting import figure
 
 import altair as alt
-from vega_datasets import data
+#from vega_datasets import data
 
 #Icône et nom de l'onglet
 st.set_page_config(page_title='Comparaison HFO vs GPL',page_icon='Logo_TotalEnergies.png', initial_sidebar_state="expanded", layout = "wide")
